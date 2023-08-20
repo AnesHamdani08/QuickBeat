05cb51a86ced4ca20cf9bbf5d70726218ab8378d
AnesHamdani08 <38377619+AnesHamdani08@users.noreply.github.com>
2023-08-21 00:44:39 +0100

    Add files via upload

---
+++ Source/QuickBeat/Controls/DragDropListBox.vb
@@ -0,0 +1,49 @@
+﻿Namespace Controls
+    Public Class DragDropListBox
+        Inherits ListBox
+
+        Shared ReadOnly Property IsDragDropModeProperty As DependencyProperty = DependencyProperty.Register("IsDragDropMode", GetType(Boolean), GetType(DragDropListBox))
+
+        Public Property IsDragDropMode As Boolean
+            Get
+                Return GetValue(IsDragDropModeProperty)
+            End Get
+            Set(value As Boolean)
+                SetValue(IsDragDropModeProperty, value)
+            End Set
+        End Property
+
+        Private Sub DragDropListBox_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+            Dim ICS As New Style(GetType(ListBoxItem))
+            ICS.BasedOn = ItemContainerStyle
+            ICS.Setters.Add(New Setter(ListBoxItem.AllowDropProperty, True))
+            ICS.Setters.Add(New EventSetter(ListBoxItem.PreviewMouseLeftButtonDownEvent, New MouseButtonEventHandler(AddressOf ListBox_Playlist_PreviewMouseLeftButtonDown)))
+            ICS.Setters.Add(New EventSetter(ListBoxItem.DropEvent, New DragEventHandler(AddressOf ListBox_Playlist_Drop)))
+            Me.ItemContainerStyle = ICS
+        End Sub
+
+        Private Sub ListBox_Playlist_PreviewMouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs)
+            If IsDragDropMode Then
+                If e.LeftButton = MouseButtonState.Pressed Then
+                    Dim dragged As ListBoxItem = TryCast(sender, ListBoxItem)
+                    DragDrop.DoDragDrop(dragged, New Object() {dragged, dragged.DataContext}, DragDropEffects.Move)
+                    e.Handled = True
+                End If
+            End If
+        End Sub
+
+        Private Sub ListBox_Playlist_Drop(sender As Object, e As DragEventArgs)
+            Dim dataS As Object() = e.Data.GetData(GetType(Object()))
+            If dataS Is Nothing Then Return
+            Dim data As Player.Metadata = dataS(1)
+            Dim i_data = ItemsControl.ItemsControlFromItemContainer(dataS(0))?.ItemContainerGenerator.IndexFromContainer(dataS(0))
+            Dim target As Player.Metadata = TryCast(sender, ListBoxItem).DataContext
+            Dim i_target = ItemsControl.ItemsControlFromItemContainer(TryCast(sender, ListBoxItem)).ItemContainerGenerator.IndexFromContainer(TryCast(sender, ListBoxItem))
+            If (i_data = i_target) Is Nothing OrElse i_data = i_target Then Return
+            Dim iType = ItemsSource.GetType
+            If iType.BaseType.IsGenericType AndAlso iType.BaseType.GetGenericTypeDefinition Is GetType(ObjectModel.ObservableCollection(Of )) Then
+                TryCast(ItemsSource, Object).Move(i_data, i_target)
+            End If
+        End Sub
+    End Class
+End Namespace
--- Source/QuickBeat/Controls/ValueSlider.vb
+++ Source/QuickBeat/Controls/ValueSlider.vb
@@ -2,14 +2,28 @@
     Public Class ValueSlider
         Inherits Grid
 
-        Property _Slider As Slider
+        Private _Title As String
+        ReadOnly Property Title As String
+            Get
+                Return _Title
+            End Get
+        End Property
+
+        Private _Unit As String
+        ReadOnly Property Unit As String
+            Get
+                Return _Unit
+            End Get
+        End Property
+
+        Private _Slider As Slider
         ReadOnly Property Slider As Slider
             Get
                 Return _Slider
             End Get
         End Property
 
-        Property _TextBlock As TextBlock
+        Private _TextBlock As TextBlock
         ReadOnly Property TextBlock As TextBlock
             Get
                 Return _TextBlock
@@ -17,25 +31,35 @@
         End Property
 
         Private Sub ValueSlider_Initialized(sender As Object, e As EventArgs) Handles Me.Initialized
+            Me.RowDefinitions.Add(New RowDefinition With {.Height = GridLength.Auto})
+            Me.RowDefinitions.Add(New RowDefinition)
             Me.ColumnDefinitions.Add(New ColumnDefinition)
             Me.ColumnDefinitions.Add(New ColumnDefinition With {.Width = GridLength.Auto})
+            Dim T_TB As New TextBlock With {.Text = Title, .Margin = New Thickness(10, 0, 0, 5)}
             Dim TB As TextBlock = If(_TextBlock, New TextBlock)
             Dim Slider As Slider = If(_Slider, New Slider With {.Margin = New Thickness(0, 0, 10, 0)})
             AddHandler Slider.ValueChanged, New RoutedPropertyChangedEventHandler(Of Double)(Sub(d, _e)
-                                                                                                 TB.Text = Math.Round(_e.NewValue, 2)
+                                                                                                 TB.Text = Math.Round(_e.NewValue, 2) & " " & Unit
                                                                                              End Sub)
+            Grid.SetColumnSpan(T_TB, 2)
             Grid.SetColumn(Slider, 0)
             Grid.SetColumn(TB, 1)
+            Grid.SetRow(T_TB, 0)
+            Grid.SetRow(Slider, 1)
+            Grid.SetRow(TB, 1)
+            Me.Children.Add(T_TB)
             Me.Children.Add(Slider)
             Me.Children.Add(TB)
             _Slider = Slider
             _TextBlock = TB
-            TB.Text = Math.Round(Slider.Value, 2)
+            TB.Text = Math.Round(Slider.Value, 2) & " " & Unit
         End Sub
 
-        Sub New(TextBlock As TextBlock, Slider As Slider)
+        Sub New(TextBlock As TextBlock, Slider As Slider, Title As String, Unit As String)
             _TextBlock = TextBlock
             _Slider = Slider
+            _Title = Title
+            _Unit = Unit
         End Sub
     End Class
 End Namespace
