301f1dd1cea21a5b35f09c5e9b81a4a3ee76e3da
AnesHamdani08 <38377619+AnesHamdani08@users.noreply.github.com>
2023-09-06 16:47:12 +0100

    Added IMediaProvider

---
+++ Source/QuickBeat/Interfaces/IMediaProvider.vb
@@ -0,0 +1,18 @@
+﻿Namespace Interfaces
+    ''' <summary>
+    ''' Provides link-refresh methods
+    ''' </summary>
+    Public Interface IMediaProvider
+        Property Name As String
+        Property Author As String
+        Property Version As Version
+        Property Token As String
+
+        ''' <summary>
+        ''' Returns a refreshed url
+        ''' </summary>        
+        ''' <returns></returns>
+        Function Fetch() As Task(Of String)
+        Function FetchThumbnail() As Task(Of ImageSource)
+    End Interface
+End Namespace
